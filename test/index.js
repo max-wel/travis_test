1d0d0d62eb42ae916e53c888c3523b94be55f16f
Your NameSneakymax <maximusekeh@gmail.com>
2018-12-28 13:59:58 +0100

    1st test

--- test/index.js
+++ test/index.js
@@ -1,7 +1,12 @@
 const expect = require('chai').expect;
+const envTest = require('../index');
 
 describe('test', function(){
   it('should return a string', function(){
     expect('boy').to.equal('boy');
+  })
+
+  it('should return a secret string', function(){
+    expect(envTest()).to.equal('yah');
   })
 })
